b8de9f0dc558985210dfac48790057481a9c8cf9
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-09 11:05:32 +0300

    cleaned up the code

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,15 +13,3 @@ class Anagram
             word_match            
     end
 end
-
-#binding.pry
-# def match (arr)
-#     word= "allergy"
-#         word = word.split("").sort
-#         word_match = arr.filter { |string| string.chars.sort == word} 
-#         word_match            
-#     end
-
-
-#  pp match(%w[gallery ballerina regally clergy largely leading])
-#  p [1,2,3]
